e66d485357cac0ee677c8ca514334c97c7661221
Kevin Fangzhou Chen <kevin@Fangzhous-MacBook-Pro.local>
2015-11-23 19:55:55 -0800

    done p1

--- hw5/queryFile.sql
+++ hw5/queryFile.sql
@@ -4,18 +4,20 @@
 
 set transaction read write;
 
-
 /*Code goes here*/
-
+SELECT name,age FROM s;
+SELECT * FROM data;
 
 commit;
 
-/*Update, Insert, Delete: Part 1: insert entries*/
+/*Update 1, Insert, Delete: Part 1: insert entries*/
 set transaction read write;
 
-
 /*Code goes here*/
-
+INSERT INTO data VALUES (6, 60);
+INSERT INTO data VALUES (10, 33);
+INSERT INTO s VALUES (6, 6, 'Michael');
+INSERT INTO s VALUES (7, 60, 'Michelle');
 
 commit;
 
@@ -27,11 +29,22 @@ select * from s;
 /*Part 2: update entries*/
 /*Put everything in 1 transaction, as seen in above part 1*/
 
-
-
 /*Code goes here*/
+set transaction read write;
 
+UPDATE data
+SET data.f2=1
+WHERE data.f1=1;
 
+UPDATE s
+SET s.age=100
+WHERE s.name='Michelle';
+
+UPDATE data
+SET data.f2=100
+WHERE data.f2=50;
+
+commit;
 
 /*========please do not remove=========*/
 select * from data;
@@ -42,8 +55,15 @@ select * from s;
 /*Put everything in 1 transaction, as seen in above part 1*/
 
 
-
 /*Code goes here*/
+set transaction read write;
+
+DELETE FROM data
+WHERE data.f1=2;
+
+DELETE FROM s;
+
+commit;
 
 
 
